f0fa90368a963541ee6f2d1341602ca0c60935d7
quietvoid <39477805+quietvoid@users.noreply.github.com>
2020-06-03 18:42:09 +0000

    Rework the packaging (#20)
    Closes #17.

--- awsmfunc/__init__.py
+++ awsmfunc/__init__.py
@@ -1,2 +1,49 @@
-from .awsmfunc import *
-from .detect import *
+from .awsmfunc import (
+    FixColumnBrightnessProtect2,
+    FixRowBrightnessProtect2,
+    FixBrightnessProtect2,
+    FixColumnBrightness,
+    FixRowBrightness,
+    ReplaceFrames,
+    bbmod,
+    bbmoda,
+    AddBordersMod,
+    BlackBorders,
+    CropResize,
+    CropResizeReader,
+    DebandReader,
+    LumaMaskMerge,
+    RGBMaskMerge,
+    ScreenGen,
+    DynamicTonemap,
+    FillBorders,
+    SelectRangeEvery,
+    FrameInfo,
+    DelFrameProp,
+    InterleaveDir,
+    ExtractFramesReader,
+    fixlvls,
+    mt_lut,
+    scale,
+    autogma,
+    UpscaleCheck,
+    Import,
+    greyscale,
+)
+
+from .detect import (
+    bandmask,
+    merge_detections,
+    banddtct,
+    detect_dirty_lines,
+    dirtdtct
+)
+
+# Aliases
+from .awsmfunc import (
+    GetPlane,
+    rfs,
+    fb,
+    cr, CR, cropresize,
+    gs, grayscale, GreyScale, GrayScale,
+)
