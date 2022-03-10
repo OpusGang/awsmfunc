d1c2331fc39f4f84a0257c0234ac3dbd1dc3f372
quietvoid <39477805+quietvoid@users.noreply.github.com>
2022-03-10 15:38:18 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import setup
 
-with open('requirements.txt') as fh:
-    install_requires = fh.read()
-
 setup(
     name='awsmfunc',
     version='1.3.1',
+    description='awesome VapourSynth functions',
     url='https://github.com/OpusGang/awsmfunc',
+    license='MIT',
     author='OpusGang',
     packages=['awsmfunc'],
     package_data={
         'awsmfunc': ['py.typed'],
     },
-    install_requires=install_requires,
+    install_requires=[
+        'VapourSynth>=57',
+        'rekt @ git+https://github.com/OpusGang/rekt.git',
+        'vsutil @ git+https://github.com/Irrational-Encoding-Wizardry/vsutil.git@956fa579406ca9edf6e0b6a834defae28efb51ce',
+    ],
     zip_safe=False,
+    python_requires='>=3.8',
 )
